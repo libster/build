cba3cd5eee8ea3068a2695c7f3f29f48693582ee
Robbie <robbie.markham@gmail.com>
2012-07-22 09:28:39 +0100

    Updated proxy_test and removed empty rvm dir

--- scripts/spec/proxy_spec.rb
+++ scripts/spec/proxy_spec.rb
@@ -1,22 +1,31 @@
 require 'watir-webdriver'
 
-
-
 describe "proxy" do
 
+	before :all do
+		@browser = Watir::Browser.new
+	end
+
+	after :all do
+		@browser.close
+	end
+
+
 	it "gets to google.com" do
-		# b = Watir::Browser.new 
-		# b.goto 'bit.ly/watir-webdriver-demo'
+		@browser.goto 'www.google.com'
 		# b.text_field(:id => 'entry_0').set 'your name'
 		# b.select_list(:id => 'entry_1').select 'Ruby'
 		# b.select_list(:id => 'entry_1').selected? 'Ruby'
 		# b.button(:name => 'submit').click
 
-		# b.text should_contain("Thank you")
+		#b.text should_contain("Thank you")
+		@browser.title.should include "Google"
+		#a.should include "test"
+	end
 
-		a = "test"
-
-		a.should include "test"
+	it "gets to Friv.com" do
+		@browser.goto 'www.friv.com'
+		@browser.title.should include "Friv"
 	end
 
 end
